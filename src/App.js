0b8eccf8cd72c09999a044fc15437f3cadd0ee33
huuthangdut <huuthang.dut@gmail.com>
2023-07-10 08:53:07 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world Test CI/CD 2
+          Hello world Test CI/CD 3
         </p>
         <a
           className="App-link"
