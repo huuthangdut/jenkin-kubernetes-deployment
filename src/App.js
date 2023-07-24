c7ea4f7466946925e8d99b019bf7fdb40f2828b0
huuthangdut <huuthang.dut@gmail.com>
2023-07-24 15:17:29 +0700

    Update src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world.
+          Hello from CI/CD
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn CI/CD
         </a>
       </header>
     </div>
