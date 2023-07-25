f650091c063f2fa8405ac92b32b1625d5f8604bb
huuthangdut <huuthang.dut@gmail.com>
2023-07-25 10:20:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world 1
+          Hello world 2
         </p>
         <a
           className="App-link"
